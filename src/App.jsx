4228bd326847e3a5278a79439321eb02d7705295
sahilnamdeo2002 <sahilnamdeo12@gmail.com>
2024-09-22 22:32:43 +0530

    Refactor CSS styles and update background color
    - Update the background color of the body and navbar to black for a more visually appealing design.
    - Remove unused import statements in App.jsx and Home.jsx.
    - Comment out the Firstsection component in App.jsx.
    - Add a new image section2.jpg to the Home component.
    - Update the CSS styles in Navbar.css and Home.css for better alignment and positioning.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
+// import Firstsection from './components/sections/firstsection'
+
 const App = () => {
   return (
-
-
     <div>
       <Navbar />
       <Home />
+      {/* <Firstsection/> */}
     </div>
 
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Home.css";
 // import {useNavigate} from "react-dom";
+import second2 from "../../images/section2.jpg";
+import home from "../../images/background.png";
+// import purple from "../../images/purple.jpg";
 const Home = () => {
 
 
@@ -10,11 +13,34 @@ const Home = () => {
 
   return (
 
-    <div className='home'>
+    <div className='fullclass'>
+      <div className='home'>
+        <img src={home} alt="background" />
 
-      <button className="button" onClick={() => changestate()}>BUY</button>
+      
+
+
+
+      </div>
+      <div className='imgg'>
+        <button className="button" onClick={() => changestate()}>BUY</button>
+        <p className='text'> From ₹119900.00*</p>
+        <p className='ff'>Apple Intelligence starting in US English later this year1</p>
+
+      </div>
+
+
+      <div className='section1'>
+       <img src={second2} alt="" />
+      </div>
+
+
+      {/* <div>
+        <img src={purple} alt="" />
+      </div> */}
+
+
     </div>
-
   )
 }
 
